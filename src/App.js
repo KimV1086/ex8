6b849e407408f17766ca9d09b665bf3cfb511cef
Vitalik Kim <kimv1086@gmail.com>
2019-12-21 11:37:02 +0600

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>test</h1>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                {/*<BrowserRouter>*/}
+                {/*    <Switch>*/}
+                {/*        <Route path="/" exact component={QuoteList}/>*/}
+                {/*        <Route path="/add" component={QuoteAdd}/>*/}
+                {/*        <Route path="/quotes/:id/edit" component={QuoteEdit}/>*/}
+                {/*        <Route path="/quotes/:categoryId" component={QuoteList}/>*/}
+                {/*    </Switch>*/}
+                {/*</BrowserRouter>*/}
+            </div>
+        );
+    }
 }
 
 export default App;
+
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <div className="Main-nav">
+            <div className="Logo">Quotes Central</div>
+            <div className="Menu">
+                <NavLink to="/">Quotes</NavLink>
+                <NavLink to="/add">Submit new quote</NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
